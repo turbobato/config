2f6ccf81bf7c2efde43502a8e302362712e1a894
Guilhem Jazeron <guilhem.jazeron@telecom-paris.fr>
2022-04-19 15:46:27 +0200

    ajout de quotes autour des expansion de variabke

--- bin/backup.sh
+++ bin/backup.sh
@@ -4,10 +4,10 @@ if [[ $# -ne 1 ]]
 then
     echo "Invalid number of arguments"
 else 
-    if [[ -d $1 ]] 
+    if [[ -d "$1" ]] 
     then
         date=$(date %F)
-        tar cvfz $1/backup_$date.tar.gz $HOME
+        tar cvfz "$1"/backup_"$date".tar.gz "$HOME"
     else 
         echo "Invalid name of directory"
     fi
